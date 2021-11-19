ca5606097140017fabb445336de2c15ef9986058
NandaPL <fpl.snf19@uea.edu.br>
2021-11-19 18:49:24 -0400

    update: update information
    Updating links via GitHub API

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'NandaPL',
+  instagram: 'nandap_l',
+  facebook: 'fernandapintolopes.lopes',
+  twitter: 'nandap_l'
+}
+
+function changeSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMedia()
+
+/* Consumindo informações de API do GitHub */
+function getGitHubProfileInfo() {
+  const urlProfile = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(urlProfile)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfo()
